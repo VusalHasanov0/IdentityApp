de431dec6690fdc9bd6a14ba96a47488aaf1f45f
VusalHasanov0 <vusalhesenov361@gmail.com>
2024-10-22 00:25:37 +0400

    created IdentityApp

---
+++ IdentityApp/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser{UserName = model.UserName,Email=model.Email};
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError item in result.Errors)
+                {
+                    ModelState.AddModelError("",item.Description);
+                }
+            }
+            return View(model);
+                
+        }
+    }
+}
